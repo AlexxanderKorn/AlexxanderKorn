c54cf10e44f6aee5baac9d53f9092db269667b0f
A. Korneev <korneevaa@vtb.ru>
2024-01-26 21:01:46 +0300

    DB handled. Keyboard refined to DB

--- InCommunityHelpBot/database.py
+++ InCommunityHelpBot/database.py
@@ -5,22 +5,12 @@ from sqlite3 import Error
 def create_connection(path):
     connection = None
     try:
-        connection = sqlite3.connect(path)
+        connection = sqlite3.connect(path, check_same_thread=False)
         print("Connection to SQLite DB successful")
     except Error as e:
         print(f"The error '{e}' occurred")
 
     return connection
-
-
-def query_execute(connection, query):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        connection.commit()
-        print("Query was executed successfully")
-    except Error as e:
-        print(f"The error '{e}' has occurred")
 
 
 def execute_read_query(connection, query):
@@ -29,134 +19,47 @@ def execute_read_query(connection, query):
     try:
         cursor.execute(query)
         result = cursor.fetchall()
-        # return result
     except Error as e:
         print(f"The error '{e}' has occurred")
     return result
 
 
-#
-# create_table_users = """
-# CREATE TABLE IF NOT EXISTS users (
-#   id INTEGER PRIMARY KEY AUTOINCREMENT,
-#   name TEXT NOT NULL,
-#   age INTEGER,
-#   gender TEXT,
-#   nationality TEXT
-# );
-# """
-#
-#
-# create_table_posts = """
-# CREATE TABLE IF NOT EXISTS posts(
-#   id INTEGER PRIMARY KEY AUTOINCREMENT,
-#   title TEXT NOT NULL,
-#   description TEXT NOT NULL,
-#   user_id INTEGER NOT NULL,
-#   FOREIGN KEY (user_id) REFERENCES users (id)
-# );
-# """
-#
-# create_table_comments = """
-# CREATE TABLE IF NOT EXISTS comments (
-#   id INTEGER PRIMARY KEY AUTOINCREMENT,
-#   text TEXT NOT NULL,
-#   user_id INTEGER NOT NULL,
-#   post_id INTEGER NOT NULL,
-#   FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
-# );
-# """
-#
-# create_table_likes = """
-# CREATE TABLE IF NOT EXISTS likes (
-#   id INTEGER PRIMARY KEY AUTOINCREMENT,
-#   user_id INTEGER NOT NULL,
-#   post_id integer NOT NULL,
-#   FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
-# );
-# """
+with open('/Users/aakorneev/PycharmProjects/AlexxanderKorn/InCommunityHelpBot/db_conn.txt') as db_f:
+    db_conn = db_f.read()
+    connection = create_connection(db_conn)
 
 
-create_table_ch = """
-CREATE TABLE IF NOT EXISTS ch (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  ch_name TEXT NOT NULL,
-  ch_address TEXT,
-  ch_how_get TEXT,
-  ch_navigation BLOB,
-  ch_url TEXT,
-  ch_prayer_time TEXT,
-  ch_phone TEXT,
-  ch_comment TEXT,
-  abbot_id INTEGER,
-  FOREIGN KEY (abbot_id) REFERENCES abbots (id),
-  UNIQUE (id, ch_name)
+class Database:
+    def __init__(self):
+        self.conn = connection
+        self.cursor = self.conn.cursor()
 
-);
-"""
+    def ch_info(self, ch_name):
+        select_ch = f"""SELECT * FROM ch JOIN abbots a ON ch.abbot_id = a.id
+        WHERE ch.ch_id_name = '{ch_name}'"""
+        ch = execute_read_query(connection, select_ch)
 
-create_table_abbots = """
-CREATE TABLE IF NOT EXISTS abbots (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  abbot_name TEXT NOT NULL,
-  abbot_rank TEXT
-);
-"""
+        return ch
 
-create_table_people = """
-CREATE TABLE IF NOT EXISTS people (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  p_abbr TEXT NOT NULL,
-  p_address TEXT,
-  p_how_get TEXT,
-  p_navigation BLOB,
-  car_id INTEGER,
-  ch_comment TEXT,
-  FOREIGN KEY (car_id) REFERENCES cars (id),
-  UNIQUE (id, p_abbr)
-);
-"""
+    def bot_buttons(self, table):
+        rec_count = execute_read_query(connection, f"""SELECT COUNT(*) FROM '{table}'""")
+        rec_count_formatted = rec_count[0][0]
+        buttons_names = {}
+        for i in range(rec_count_formatted):
+            i = i + 1
+            if table == 'ch':
+                tab_values = execute_read_query(connection, f"""SELECT ch_id_name, ch_name FROM ch WHERE id = '{i}'""")[0]
+            elif table == 'people':
+                tab_values = execute_read_query(connection, f"""SELECT p_abbr FROM ch WHERE id = '{i}'""")[
+                    0]
+            else:
+                raise ValueError(f'Table {table} is not defined')
+            buttons_names.update({tab_values[0]: tab_values[1]})
+        # print(buttons_names)
+        return buttons_names
 
-create_table_cars = """
-CREATE TABLE IF NOT EXISTS cars (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  car_model TEXT NOT NULL,
-  car_number TEXT
-);
-"""
 
-# insert_ch = """
-# INSERT INTO
-#   ch (ch_name, ch_address, ch_how_get, ch_navigation, ch_url, ch_prayer_time, ch_phone, ch_comment, abbot_id)
-# VALUES
-#   ('Рождества Иоанна Предтечи на Пресне', 'м. Краснопресненская, Малый Предтеченский переулок, д. 2', 'Выход 1 из метро',
-#   '/Users/aakorneev/Documents/AK/Bot/IoannPr_how_to_get.jpg',
-#   'https://ioannpr.ru/raspisanie-bogosluzhenij', '09:30', '+7-499-255-65-72', '', 1);
-# """
+# database = Database()
 
-# insert_abb = """
-# INSERT INTO
-#   abbots (abbot_name, abbot_rank)
-# VALUES
-#   ('Антоний (Севрюк)', 'митр. ');
-# """
-
-select_ch = "SELECT * FROM ch JOIN abbots ON ch.abbot_id = abbots.id;"
-select_abbot = "SELECT * FROM abbots;"
-
-connection = create_connection("/Users/aakorneev/PycharmProjects/AlexxanderKorn/InCommunityHelpBot/info_bot.sqlite")
-
-# query_execute(connection, create_table_ch)
-# query_execute(connection, create_table_abbots)
-
-# query_execute(connection, insert_ch)
-# query_execute(connection, insert_abb)
-
-ch = execute_read_query(connection, select_ch)
-for i in ch:
-    print(i)
-
-# abbot = execute_read_query(connection, select_abbot)
-# for i in abbot:
-#     print(i)
+# ch_buttons_set('ch')
 
--- InCommunityHelpBot/main.py
+++ InCommunityHelpBot/main.py
@@ -1,199 +1,126 @@
-
-
 import telebot
-#from flask import Flask, request
+# from flask import Flask, request
 from telebot import types
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, Update
-#from telebot.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
+# from telebot.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
 from telebot.util import quick_markup
 from urllib3.util import url
-from termcolor import colored
+# from termcolor import colored
+from database import Database
 
-token = 'token'
+"""
+    . Запуск бота: python <имя py-файла>
+    . Данные содержатся в БД
+"""
+
+database = Database()
+
+with open('/Users/aakorneev/PycharmProjects/AlexxanderKorn/InCommunityHelpBot/bot_tok.txt') as f:
+    token = f.read()
+
 InCommunityHelpBot = telebot.TeleBot(token)
 
 
-#app = Flask(__name__)
+# app = Flask(__name__)
 
-'''
-Вариант постоянного меню внизу окна
-'''
+def out_yellow(text):
+    pp = "\033[33m {}".format(text)
 
-# @InCommunityHelpBot.message_handler(commands=['start', 'button'])
-# def start_bot(message):
-#     start_mess = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nЧто хотелось бы узнать?"
-#     #    InCommunityHelpBot.send_message(message.chat.id, start_mess, parse_mode='html')
-#
-#     # @InCommunityHelpBot.message_handler(commands=['button'])
-#     # def first_choice(message):
-#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#     button1 = types.KeyboardButton("Инфо о храмах")
-#     button2 = types.KeyboardButton("Наши адреса")
-#     markup.add(button1, button2)
-#
-#     InCommunityHelpBot.send_message(message.chat.id, start_mess, parse_mode='html', reply_markup=markup)
-#
-#
-# @InCommunityHelpBot.message_handler(content_types='text')
-# def message_reply(message):
-#     if message.text == "Инфо о храмах":
-#         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#         button_ioanpr = types.KeyboardButton("Рождества Иоанна Предтечи на Пресне")
-#         button_irina = types.KeyboardButton("Вмц. Ирины на Бауманской")
-#         button_flora = types.KeyboardButton("Флора и Лавра")
-#         markup.add(button_irina, button_ioanpr)
-#         markup.add(button_irina)
-#         markup.add(button_flora)
-#         InCommunityHelpBot.send_message(message.chat.id, 'Выберите храм', reply_markup=markup)
-#     elif message.text == "Наши адреса":
-#         InCommunityHelpBot.send_message(message.chat.id, 'Здесь будут наши адреса')
-#
-#
-# @InCommunityHelpBot.message_handler(content_types='text')
-# def message_reply(message):
-#     if message.text == "Рождества Иоанна Предтечи на Пресне":
-#         InCommunityHelpBot.send_message(message.chat.id,
-#                                         f"Адрес: м. Краснопресненская, Малый Предтеченский переулок, д. 2 \nНачало богослужений: 09:30 \nСайт: {'url': 'https://ioannpr.ru/raspisanie-bogosluzhenij'}")
-#     elif message.text == "Вмц. Ирины на Бауманской":
-#         InCommunityHelpBot.send_message(message.chat.id, 'Здесь будут наши адреса')
+    return pp
 
 
-'''
-Вариант с inline-меню
-'''
+def bot_buttons_set(table: str):
+    bot_buttons = (database.bot_buttons(table))
+    menu_buttons = []
+
+    for key in bot_buttons.keys():
+        key = InlineKeyboardButton(text=bot_buttons[key], callback_data=key)
+        menu_buttons.append(key)
+
+    return list(bot_buttons), menu_buttons
+
+
+def ch_data_set(ch_d: str):
+    ch_data = (database.ch_info(ch_d)[0])
+    # pr_start = out_yellow("Начало богослужений:")
+    ch_set = (
+        f'<u><b>Храм:</b></u>    {ch_data[1]}\n'
+        f'<b>Настоятель:</b><i> {ch_data[12], ch_data[13]}</i>\n\n'
+
+        f'<u><b>Адрес:</b></u><i>   {ch_data[2]}</i>\n'
+        f'<b>Как добраться:</b><i> {ch_data[3]}</i>\n\n'
+
+        f'<b>Начало богослужений:</b><i> {ch_data[6]}</i>\n'
+        f'<b>Сайт:</b><i>    {ch_data[5]}</i>')
+    return ch_set
+
+
 button_church = InlineKeyboardButton(text='Инфо о храмах', callback_data='church')
 button_address = InlineKeyboardButton(text='Наши адреса', callback_data='address')
 main_keyboard = InlineKeyboardMarkup().add(button_church, button_address)
 
 button_back = InlineKeyboardButton(text='Вернуться', callback_data='back')
+keyboard_back = InlineKeyboardMarkup([[button_back]])
 
-button_ch_ioann = InlineKeyboardButton(text='Рождества Иоанна Предтечи на Пресне', callback_data='ioann')
-button_ch_irina = InlineKeyboardButton(text='Вмц. Ирины на Бауманской', callback_data='irina')
-button_site = InlineKeyboardButton(text='Site', url="https://core.telegram.org/bots/api")
-keyboard_church = InlineKeyboardMarkup([
-    [button_ch_ioann],
-    [button_ch_irina],
-    [button_back]
-])
-#keyboard_church = InlineKeyboardMarkup().add(button_ch_ioann, button_ch_irina, button_back)
 
 button_addr_AK = InlineKeyboardButton(text='АК', callback_data='AK_adr')
 button_addr_IK = InlineKeyboardButton(text='ИК', callback_data='IK_adr')
+button_addr_KB = InlineKeyboardButton(text='КБ', callback_data='KB_adr')
 keyboard_address = InlineKeyboardMarkup([
-    [button_addr_AK], [button_addr_IK],
+    [button_addr_AK], [button_addr_IK], [button_addr_IK],
     [button_back]
 ])
 
-#
-#
-# # Pre-assign menu text
-# FIRST_MENU = "<b>Menu 1</b>\n\nИнфо по храмам"
-# SECOND_MENU = "<b>Menu 2</b>\n\nИнфо по адресам"
-#
-# # Pre-assign button text
-# NEXT_BUTTON = "Храмы"
-# BACK_BUTTON = "Наши адреса"
-# TUTORIAL_BUTTON = "Назад"
-#
-# # Build keyboards
-# FIRST_MENU_MARKUP = InlineKeyboardMarkup([[
-#     InlineKeyboardButton(NEXT_BUTTON, callback_data=NEXT_BUTTON)
-# ]])
-# SECOND_MENU_MARKUP = InlineKeyboardMarkup([
-#     [InlineKeyboardButton(BACK_BUTTON, callback_data=BACK_BUTTON)],
-#     [InlineKeyboardButton(TUTORIAL_BUTTON, url="https://core.telegram.org/bots/api")]
-# ])
 
 @InCommunityHelpBot.message_handler(commands=['start'])
 def start_bot(message):
     start_mess = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nЧто хотелось бы узнать?"
 
-    # markup = types.InlineKeyboardMarkup()
-    # button_church = types.InlineKeyboardButton(text='Инфо о храмах', callback_data='church')
-    # button_address = types.InlineKeyboardButton(text='Наши адреса', callback_data='address')
-    # markup.add(button_church, button_address)
-
     InCommunityHelpBot.send_message(message.chat.id, start_mess, parse_mode='html', reply_markup=main_keyboard)
 
 
-# @InCommunityHelpBot.message_handler(commands=['button'])
-# def first_bot(message):
-#
-#     if message.callback_data == "church":
-#         church_mess = f"Адреса храмов \nПройти"
-#
-#         markup = types.InlineKeyboardMarkup()
-#         button_ioann = types.InlineKeyboardButton(text='Рождества Иоанна Предтечи на Пресне', callback_data='ioann')
-#         button_irina = types.InlineKeyboardButton(text='Вмц. Ирины', callback_data='irina')
-#         markup.add(button_ioann)
-#         markup.add(button_irina)
-#
-#         InCommunityHelpBot.send_message(message.chat.id, church_mess, parse_mode='html', reply_markup=markup)
-#
 @InCommunityHelpBot.callback_query_handler(func=lambda call: True)
 def response_first(call):
+    info_church_menu = bot_buttons_set('ch')
+    ch_d = info_church_menu[0]
+    info_keyboard = InlineKeyboardMarkup([info_church_menu[1]])
+
     if call.message:
         if call.data == "church":
             second_mess = "Информация по храмам"
-            # markup = types.InlineKeyboardMarkup()
-            # button_ioann = types.InlineKeyboardButton(text='Рождества Иоанна Предтечи на Пресне', callback_data='ioann')
-            # button_irina = types.InlineKeyboardButton(text='Вмц. Ирины на Бауманской', callback_data='irina')
-            # markup.add(button_ioann)
-            # markup.add(button_irina)
-            #
-            # InCommunityHelpBot.send_message(call.message.chat.id, second_mess, parse_mode='html', reply_markup=markup)
 
-            # markup = quick_markup({
-            #     'Рождества Иоанна Предтечи на Пресне': {'url': 'https://ioannpr.ru/raspisanie-bogosluzhenij'},
-            #     'Вмц. Ирины на Бауманской': {'url': 'http://храм-ирины.рф/расписание-богослужений/'},
-            #     'Back': {'callback_data': 'church'},
-            #     'Инфо': {'callback_data': 'Инфо о храмах'},
-            #     'Inline_query': {'switch_inline_query_current_chat': '???'}
-            # }, row_width=1)
-            InCommunityHelpBot.send_message(call.message.chat.id, second_mess, reply_markup=keyboard_church)
+            InCommunityHelpBot.send_message(call.message.chat.id,
+                                            second_mess,
+                                            parse_mode='html',
+                                            reply_markup=info_keyboard)
 
         elif call.data == "address":
             address_mess = "Информация по адресам"
-            InCommunityHelpBot.send_message(call.message.chat.id, address_mess, parse_mode='html', reply_markup=keyboard_address)
+            InCommunityHelpBot.send_message(call.message.chat.id,
+                                            address_mess,
+                                            parse_mode='html',
+                                            reply_markup=keyboard_address)
 
-        if call.data == 'ioann':
-            ioann_info = f"<u>Храм Рождества Иоанна Предтечи</u>\n\n" \
-                         f"<b><i>Адрес:</i></b> м. Краснопресненская, Малый Предтеченский переулок, д. 2 \n" \
-                         f"<b><i>Начало богослужений:</i></b> 09:30 \n" \
-                         f"<b><i>Сайт:</i></b> https://ioannpr.ru/raspisanie-bogosluzhenij"
-            InCommunityHelpBot.send_message(call.message.chat.id, ioann_info, parse_mode='html')
-        elif call.data == 'irina':
-            irina_info = f"Храм вмц. Ирины\n\n" \
-                         f"<b><i>Адрес:</i></b> м. Бауманская,..., д. 2. \n" \
-                         f"<b><i>Начало богослужений:</i></b> 10:00 \n" \
-                         f"<b><i>Сайт:</i></b> http://храм-ирины.рф/расписание-богослужений/"
-            InCommunityHelpBot.send_message(call.message.chat.id, irina_info, parse_mode='html')
+        if call.data in ch_d:
+            InCommunityHelpBot.send_message(call.message.chat.id,
+                                            ch_data_set(call.data),
+                                            parse_mode='html',
+                                            reply_markup=keyboard_back)
+
+        elif call.data == "back":
+            second_mess = "Информация по храмам"
+            InCommunityHelpBot.send_message(call.message.chat.id, second_mess, reply_markup=info_keyboard)
 
         if call.data == 'AK_adr':
             AK_info = f"Адрес: м. Рассказовка, бульвар Андрея Тарковского, д.4, кв.429, 7 подъезд, д/ф 429, 3 эт.\n\n" \
                       f"Как добраться: 1 вагон из центра. Если на автобусе, то налево, 50 м до ост. Авт 333, 3-я остановка\n" \
                       f"Если пешком: выход из метро направо, далее - прямо и прямо вдоль дороги метров 500 до светофора. На светофоре налево будет входная группа.\n Для прохода нужен пропуск"
             InCommunityHelpBot.send_message(call.message.chat.id, AK_info, parse_mode='html')
-#
-# # @InCommunityHelpBot.callback_query_handler(func=lambda call: True)s
-# def response_second(call):
-#     if call.message:
-#         if call.data == "ioann":
-#             ioann_mess = "Адрес: м. Краснопресненская, Малый Предтеченский переулок, д. 2."
-#             InCommunityHelpBot.send_message(call.message.chat.id, ioann_mess, parse_mode='html')
-#
-#         elif call.data == "irina":
-#             irina_mess = "Адрес: м. Бауманская,..., д. 2. Начало богослужений: 10:00 Сайт: 'http://храм-ирины.рф/расписание-богослужений/'"
-#             InCommunityHelpBot.send_message(call.message.chat.id, irina_mess, parse_mode='html')
 
-# f"Адрес: м. Краснопресненская, Малый Предтеченский переулок, д. 2 "
-# f"\nНачало богослужений: 09:30 "
-# f"\nСайт: 'https://ioannpr.ru/raspisanie-bogosluzhenij'"
 
 if __name__ == '__main__':
     InCommunityHelpBot.polling(none_stop=True)
-    #InCommunityHelpBot.infinity_polling()
-
+    # InCommunityHelpBot.infinity_polling()
 
 # @app.route("/" + token, methods=['POST'])
 # def getMessage():
